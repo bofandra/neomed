6b2e85fc7bef78f9ae0c8bbe26aff7283abb2601
bofandra.muhammad <bofandra.muhammad@dto.kemkes.go.id>
2025-06-08 16:23:29 +0700

    add demo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import DemoPage from "./components/DemoPage"; // You'll need to create this if it doesn't exist yet
+
 
 export default function App() {
   return (
